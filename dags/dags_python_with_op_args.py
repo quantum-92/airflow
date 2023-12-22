ec9b5a4649841af9b4c22d57ffa7896c24db597d
Lee Hyeokhee <hyeokhee@gmail.com>
2023-12-22 22:57:32 +0900

    python with op

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,21 @@
+import datetime
+import pendulum
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from common.common_func import regist
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 12, 1, tz="Asia/Seoul"),
+    catchup=False,
+    tags=["python_with_op", "슬통생"]
+) as dag:
+
+    regist_t1 = PythonOperator(
+        task_id = "regist_t1",
+        python_callable = regist,
+        op_args = ['kjkim', 'male', 'kr', 'seoul']
+    )
+    
+    regist_t1
