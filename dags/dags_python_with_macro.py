eca6309d44f9e39dcda3d854d6df75b8298b9736
quantum-92 <ehyeokhee@gmail.com>
2023-12-25 04:11:39 +0900

    .

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -1,9 +1,10 @@
+import datetime
 import pendulum
 from airflow import DAG
 from airflow.decorators import task
 
 with DAG(
-    dag_id="dags_python_with_macro",
+    dag_id="dags_python_with_macro_1",
     schedule="10 0 * * *",
     start_date=pendulum.datetime(2023, 12, 1, tz="Asia/Seoul"),
     catchup=False,
@@ -11,12 +12,12 @@ with DAG(
 ) as dag:
     
     @task(task_id = "task_using_macro",
-          templates_dict = {'start_date': '{{(data_interval_start.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(months = -1, day = 1)) | ds}}',
-                           'end_date': '{{data_interval_end.in_timezone("Asia/Seoul") + macros.dateutil.relativedelta.relativedelta(days = -1) | ds}}'})
+          template_dict = {'start_date': '{{(data_interval_start.to_timezone("Asia/Seoul") + macros.datetimeutlils.relativedelta.relativedelta(months = -1, day = 1)) | ds}}',
+                           'end_date': '{{data_interval_end.to_timezone("Asia/Seoul") + macros.datetimeutlils.relativedelta.relativedelta(days = -1)) | ds}}'})
     def get_datetime_macro(**kwargs):
-        templates_dict = kwargs['template_dict'] or {}
-        start_date = templates_dict['start_date'] or "no start_date"
-        end_date = templates_dict['end_date'] or "no end_date"
+        template_dict = kwargs['template_dict'] or {}
+        start_date = template_dict['start_date'] or "no start_date"
+        end_date = template_dict['end_date'] or "no end_date"
         print(start_date)
         print(end_date)
     
@@ -24,9 +25,9 @@ with DAG(
     def get_datetime_direct(**kwargs):
         from dateutil.relativedelta import relativedelta
         
-        data_interval_end = kwargs['data_interval_end'].in_timzone("Asia/Seoul")
-        start_date = data_interval_end + relativedelta(months = -1, day = 1)
-        end_date = data_interval_end + relativedelta(day = 1) + relativedelta(months = -1)
+        date_time_interval_end = kwargs['date_interval_end'].to_timzone("Asia/Seoul")
+        start_date = date_time_interval_end + relativedelta(months = -1, day = 1)
+        end_date = date_time_interval_end + relativedelta(days = -1)
         print(start_date.strftime("%Y-%m-%d"))
         print(end_date.strftime("%Y-%m-%d"))
         
