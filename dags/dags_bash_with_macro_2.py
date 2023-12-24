a7bb81f8f124cb904da4dfe1c23da0618e0e60d9
quantum-92 <ehyeokhee@gmail.com>
2023-12-25 00:50:34 +0900

    bash op with macro

--- dags/dags_bash_with_macro_2.py
+++ dags/dags_bash_with_macro_2.py
@@ -13,10 +13,10 @@ with DAG(
         task_id = "bash_t2",
         env = {
             'START_DATE': '{{(data_interval_end.in_timezone("Asia/Seoul") \
-                - macros.dateutil.relativedelta.relativedelta(days = -19)) \
+                - macros.dateutil.relativedelta.relativedelta(days = 19)) \
                 | ds}} ',
             'END_DATE': '{{(data_interval_end.in_timezone("Asia/Seoul") \
-                - macros.dateutil.relativedelta.relativedelta(days = -14)) \
+                - macros.dateutil.relativedelta.relativedelta(days = 14)) \
                 | ds }}'
         },
         bash_command = 'echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
