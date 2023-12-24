4cf0d0b0ab4b0406ee7a080509bde6efaf53b699
Lee Hyeokhee <hyeokhee@gmail.com>
2023-12-25 03:22:32 +0900

    dags python with macro

--- dags/dags_python_with_macro.py
+++ dags/dags_python_with_macro.py
@@ -10,18 +10,18 @@ with DAG(
     tags=["example", "example2"]
 ) as dag:
     
-    @task(task_id = "dags_python_with_macro_1",
-          template_dict = {'start_date': '{{(data_interval_start.to_timezone("Asia/Seoul") + datetimeutlils.relativedelta.relativedelta(months = -1, day = 1)) | ds}}',
-                           'end_date': '{{data_interval_end.to_timezone("Asia/Seoul") + datetimeutlils.relativedelta.relativedelta(days = -1)) | ds}}'})
-    def datetime_func1(**kwargs):
+    @task(task_id = "task_using_macro",
+          template_dict = {'start_date': '{{(data_interval_start.to_timezone("Asia/Seoul") + macros.datetimeutlils.relativedelta.relativedelta(months = -1, day = 1)) | ds}}',
+                           'end_date': '{{data_interval_end.to_timezone("Asia/Seoul") + macros.datetimeutlils.relativedelta.relativedelta(days = -1)) | ds}}'})
+    def get_datetime_macro(**kwargs):
         template_dict = kwargs['template_dict'] or {}
         start_date = template_dict['start_date'] or "no start_date"
         end_date = template_dict['end_date'] or "no end_date"
         print(start_date)
         print(end_date)
     
-    @task(task_id = "dags_python_with_macro_2")
-    def datetime_func2(**kwargs):
+    @task(task_id = "task_direct_calc")
+    def get_datetime_direct(**kwargs):
         from dateutil.relativedelta import relativedelta
         
         date_time_interval_end = kwargs['date_interval_end'].to_timzone("Asia/Seoul")
@@ -30,4 +30,4 @@ with DAG(
         print(start_date.strftime("%Y-%m-%d"))
         print(end_date.strftime("%Y-%m-%d"))
         
-    datetime_func1() >> datetime_func2()
+    get_datetime_macro() >> get_datetime_direct()
